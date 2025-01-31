3dda1b6182c61c2f0cbeeec7d329b64505f3b556
Mariya Kryvitskaya <masha.oath@gmail.com>
2025-01-31 23:12:04 +0300

    feat: replace native datalist with custom dropdown

--- search-app/src/app/features/search/components/search-input/search-input.component.ts
+++ search-app/src/app/features/search/components/search-input/search-input.component.ts
@@ -1,20 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Output, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { selectSuggestedQueries } from '../../store/search.selectors';
-import { AsyncPipe, NgFor } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-search-input',
   standalone: true,
-  imports: [FormsModule, AsyncPipe, NgFor],
+  imports: [FormsModule, AsyncPipe, NgFor, NgIf],
   templateUrl: './search-input.component.html',
   styleUrl: './search-input.component.scss',
 })
 export class SearchInputComponent {
   query = '';
+  showSuggestions = false;
+
   @Output() queryChange = new EventEmitter<string>();
+  @ViewChild('searchInput') searchInput!: ElementRef;
 
   suggestedQueries$: Observable<string[]>;
 
@@ -24,5 +27,18 @@ export class SearchInputComponent {
 
   onSearchChange(query: string) {
     this.queryChange.emit(query);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    if (!this.searchInput.nativeElement.contains(event.target)) {
+      this.showSuggestions = false;
+    }
+  }
+
+  selectSuggestion(suggestion: string) {
+    this.query = suggestion;
+    this.showSuggestions = false;
+    this.onSearchChange(suggestion);
   }
 }
